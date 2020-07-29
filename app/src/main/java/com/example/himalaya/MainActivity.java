67f1710846d0e5dd7dc19097752ff1e0c110716a
aisen1128 <772472531@qq.com>
2020-07-29 14:55:59 +0800

    test

--- app/src/main/java/com/example/himalaya/MainActivity.java
+++ app/src/main/java/com/example/himalaya/MainActivity.java
@@ -75,6 +75,7 @@ public class MainActivity extends FragmentActivity {
         magicIndicator.setNavigator(commonNavigator);
         ViewPagerHelper.bind(magicIndicator,mContentPager);
 
+        //test
     }
 
 }
